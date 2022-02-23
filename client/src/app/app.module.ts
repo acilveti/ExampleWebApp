d52d4ac7d6113148569f16d012e7f020155bc53a
acilveti <ander.cilveti.alvarez@gmail.com>
2022-02-23 18:29:00 +0100

    initial commit - walking skeleton

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,24 @@ import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { MainbodyComponent } from './mainbody/mainbody.component';
+import { TechstackComponent } from './content/techstack/techstack.component';
+import { TechComponent } from './content/techstack/tech/tech.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    MainbodyComponent,
+    TechstackComponent,
+    TechComponent,
+    
   ],
   imports: [
     BrowserModule,
